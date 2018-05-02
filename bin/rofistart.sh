fd8b96ce1b0a0e2c4928a2b404559b08ec4aae5b
Daniel McCoshen <upstagecoronet@gmail.com>
2018-05-01 22:32:57 -0600

    first commit of core config files

---
+++ bin/rofistart.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rofi -color-window "#232729, #232729, #000000" -color-normal "#232729, #63696B, #232729, #232729, #C3C4C3" -color-active "#232729, #33393B, #232729, #232729, #33393B" -color-urgent "#232729, #f3f4f5, #232729, #262729, #f3f4f5" -show run -width 100 -lines 10 -opacity 90 -font "LiberationMono-Bold 15"
+
---
+++ bin/thingy.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+while :
+do
+xdotool key BackSpace
+sleep 10
+xdotool key y
+sleep 10
+done
